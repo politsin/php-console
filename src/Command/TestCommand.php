43b815827bc074e14af5eb877363b583b836cd5a
Anatoly Politsin <politsin@gmail.com>
2023-09-07 20:56:21 +0000

    x

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -34,7 +34,7 @@ class TestCommand extends Command {
    * Exec.
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-
+    $number = 1;
     $io = new SymfonyStyle($input, $output);
     $io->section('Table');
     $table = new Table($output);
@@ -60,39 +60,39 @@ class TestCommand extends Command {
     }
     // Ensures that the progress bar is at 100%.
     $progressBar->finish();
-    if (false) {
-    $io->section('Echo cmd');
-    $command = $this->getApplication()->find('test:echo');
-    $arguments = [
-      'command' => 'test:echo',
-      'text'    => 'Fabien',
+    if (FALSE) {
+      $io->section('Echo cmd');
+      $command = $this->getApplication()->find('test:echo');
+      $arguments = [
+        'command' => 'test:echo',
+        'text'    => 'Fabien',
       // '--yell'  => TRUE,
-    ];
-    $greetInput = new ArrayInput($arguments);
-    $returnCode = $command->run($greetInput, $output);
+      ];
+      $greetInput = new ArrayInput($arguments);
+      $returnCode = $command->run($greetInput, $output);
 
-    $io->title('OutputFormatterStyle');
-    $outputStyle = new OutputFormatterStyle('red', 'yellow', ['bold', 'blink']);
-    $output->getFormatter()->setStyle('fire', $outputStyle);
-    $output->writeln('<fire>foo</>');
-    $output->writeln('<fg=green>foo</>');
-    $output->writeln('<fg=black;bg=cyan>foo</>');
-    $output->writeln('<bg=yellow;options=bold>foo</>');
-    $output->writeln('<options=bold,underscore>foo</>');
-    $output->writeln('<href=https://symfony.com>Symfony Homepage</>');
+      $io->title('OutputFormatterStyle');
+      $outputStyle = new OutputFormatterStyle('red', 'yellow', ['bold', 'blink']);
+      $output->getFormatter()->setStyle('fire', $outputStyle);
+      $output->writeln('<fire>foo</>');
+      $output->writeln('<fg=green>foo</>');
+      $output->writeln('<fg=black;bg=cyan>foo</>');
+      $output->writeln('<bg=yellow;options=bold>foo</>');
+      $output->writeln('<options=bold,underscore>foo</>');
+      $output->writeln('<href=https://symfony.com>Symfony Homepage</>');
 
-    $io->text('Lorem ipsum dolor sit amet');
-    $io->text([
-      'Lorem ipsum dolor sit amet',
-      'Consectetur adipiscing elit',
-      'Aenean sit amet arcu vitae sem faucibus porta',
-    ]);
-    $io->listing([
-      'Element #1 Lorem ipsum dolor sit amet',
-      'Element #2 Lorem ipsum dolor sit amet',
-      'Element #3 Lorem ipsum dolor sit amet',
-    ]);
-    $io->definitionList(
+      $io->text('Lorem ipsum dolor sit amet');
+      $io->text([
+        'Lorem ipsum dolor sit amet',
+        'Consectetur adipiscing elit',
+        'Aenean sit amet arcu vitae sem faucibus porta',
+      ]);
+      $io->listing([
+        'Element #1 Lorem ipsum dolor sit amet',
+        'Element #2 Lorem ipsum dolor sit amet',
+        'Element #3 Lorem ipsum dolor sit amet',
+      ]);
+      $io->definitionList(
       'This is a title',
       ['foo1' => 'bar1'],
       ['foo2' => 'bar2'],
@@ -100,31 +100,29 @@ class TestCommand extends Command {
       new TableSeparator(),
       'This is another title',
       ['foo4' => 'bar4']
-    );
+      );
 
-    $io->note('Lorem ipsum dolor sit amet');
-    $io->caution('=)');
-    $io->ask('Number of workers to start', 1, function ($number) {
-      if (!is_numeric($number)) {
-        throw new \RuntimeException('You must type a number.');
-      }
-      return (int) $number;
-    });
-    $io->success([
-      'Lorem ipsum dolor sit amet',
-      'Consectetur adipiscing elit',
-    ]);
-    $io->warning("start $number");
+      $io->note('Lorem ipsum dolor sit amet');
+      $io->caution('=)');
+      $io->ask('Number of workers to start', 1, function ($number) {
+        if (!is_numeric($number)) {
+          throw new \RuntimeException('You must type a number.');
+        }
+        return (int) $number;
+      });
+      $io->success([
+        'Lorem ipsum dolor sit amet',
+        'Consectetur adipiscing elit',
+      ]);
+      $io->warning("start $number");
 
-    $io->title('Choose one');
-    $question = new ChoiceQuestion(
+      $io->title('Choose one');
+      $question = new ChoiceQuestion(
       'Please select your favorite color (defaults to red)',
       ['red', 'blue', 'yellow'],
       0
-    );
-    $question->setErrorMessage('Color %s is invalid.');
-    $color = $helper->ask($input, $output, $question);
-    $output->writeln("You have just selected: $color");
+      );
+      $question->setErrorMessage('Color %s is invalid.');
     }
 
     return 0;
